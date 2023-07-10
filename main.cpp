1c9ca279fe95988189ee56f9328b827846051802
AaryaGadekar <aaryagadekar01@gmail.com>
2023-07-10 18:41:37 +0530

    first commit

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+    // Image
+
+    const int image_width = 256;
+    const int image_height = 256; 
+
+    // Render 
+
+    cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j = image_height-1; j >= 0; --j){
+        cerr << "\rScanlines remaining: " << j << ' ' << flush;
+        for (int i = 0; i < image_width; ++i){
+            auto r = double(i)/(image_width-1);
+            auto g = double(j)/(image_height-1);
+            auto b = 0.25;
+
+            int ir = static_cast<int>(255.999 * r);
+            int ig = static_cast<int>(255.999 * g);
+            int ib = static_cast<int>(255.999 * b);
+
+            cout << ir << ' ' << ig << ' ' << ib << '\n';
+
+        }
+    }
+    cerr << "\nDone.\n";
+}
