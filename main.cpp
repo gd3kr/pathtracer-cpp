fb41c392e8deb7d04f118c50ff86d2475c044eb6
AaryaGadekar <aaryagadekar01@gmail.com>
2023-07-11 18:58:05 +0530

    antialiasing

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include "color.h"
 #include "hittable_list.h"
 #include "sphere.h"
-
+#include "camera.h"
 
 #include <iostream>
 
@@ -25,6 +25,7 @@ int main(){
     const auto aspect_ratio = 16.0/9.0;
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width/aspect_ratio);
+    const int samples_per_pixel = 100;
 
     // World
     hittable_list world;
@@ -33,15 +34,8 @@ int main(){
 
 
     // Camera
+    camera cam;
 
-    auto viewport_height = 2.0;
-    auto viewport_width = aspect_ratio * viewport_height;
-    auto focal_length = 1.0;
-
-    auto origin = point3(0,0,0);
-    auto horizontal = vec3(viewport_width, 0, 0);
-    auto vertical = vec3(0, viewport_height, 0);
-    auto lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
 
     // Render 
 
@@ -50,11 +44,14 @@ int main(){
     for (int j = image_height-1; j >= 0; --j){
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i){
-            auto u = double(i) / (image_width-1);
-            auto v = double(j) / (image_height-1);
-            ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
-            color pixel_color = ray_color(r, world);
-            write_color(std::cout, pixel_color);   
+            color pixel_color(0, 0, 0);
+            for (int s = 0; s < samples_per_pixel ; ++s){
+              auto u = (i + random_double()) / (image_width-1);
+              auto v = (j + random_double()) / (image_height-1);
+              ray r = cam.get_ray(u, v);
+              pixel_color += ray_color(r, world);           
+            }
+            write_color(std::cout, pixel_color, samples_per_pixel);   
        }
     }
     std::cerr << "\nDone.\n";
--- rtweekend.h
+++ rtweekend.h
@@ -31,6 +31,12 @@ inline double random_double(double min, double max) {
     return min + (max-min)*random_double();
 }
 
+inline double clamp(double x, double min, double max) {
+    if (x < min) return min;
+    if (x > max) return max;
+    return x;
+}
+
 // Common Headers
 
 #include "ray.h"
