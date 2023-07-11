bd16296604663d277171854fda2c0a80e33c85b8
AaryaGadekar <aaryagadekar01@gmail.com>
2023-07-11 08:07:16 +0530

    color.h, ray.h and updated main

--- main.cpp
+++ main.cpp
@@ -1,32 +1,48 @@
+#include "color.h"
+#include "vec3.h"
+#include "ray.h"
+
+
 #include <iostream>
 
-using namespace std;
+color ray_color(const ray& r){
+    vec3 unit_direction = unit_vector(r.direction());
+    auto t = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-t) * color(1.0, 1.0, 1.0) + t*(color(0.5, 0.7, 1.0));
+}
+
+
 
 
 int main(){
     // Image
+    const auto aspect_ratio = 16.0/9.0;
+    const int image_width = 400;
+    const int image_height = static_cast<int>(image_width/aspect_ratio);
 
-    const int image_width = 256;
-    const int image_height = 256; 
+    // Camera
+
+    auto viewport_height = 2.0;
+    auto viewport_width = aspect_ratio * viewport_height;
+    auto focal_length = 1.0;
+
+    auto origin = point3(0,0,0);
+    auto horizontal = vec3(viewport_width, 0, 0);
+    auto vertical = vec3(0, viewport_height, 0);
+    auto lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
 
     // Render 
 
-    cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = image_height-1; j >= 0; --j){
-        cerr << "\rScanlines remaining: " << j << ' ' << flush;
+        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i){
-            auto r = double(i)/(image_width-1);
-            auto g = double(j)/(image_height-1);
-            auto b = 0.25;
-
-            int ir = static_cast<int>(255.999 * r);
-            int ig = static_cast<int>(255.999 * g);
-            int ib = static_cast<int>(255.999 * b);
-
-            cout << ir << ' ' << ig << ' ' << ib << '\n';
-
-        }
+        auto u = double(i) / (image_width-1);
+        auto v = double(j) / (image_height-1);
+        color pixel_color = ray_color(r);
+         write_color(std::cout, pixel_color);   
+       }
     }
     cerr << "\nDone.\n";
 }
