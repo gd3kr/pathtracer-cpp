f9f5f41b19a7ad19b3189ca8e9e75d6ec6615ade
AaryaGadekar <aaryagadekar01@gmail.com>
2023-07-11 19:21:14 +0530

    diffuse complete

--- main.cpp
+++ main.cpp
@@ -7,10 +7,16 @@
 
 #include <iostream>
 
-color ray_color (const ray& r, const hittable& world) {
+color ray_color (const ray& r, const hittable& world, int depth) {
     hit_record rec;
+
+    // If ray bounce limit exceeded then no more light is gathered.
+
+    if (depth <= 0) return color(0,0,0);
+
     if (world.hit(r, 0, infinity, rec)){
-        return 0.5 * (rec.normal + color(1, 1, 1));
+        point3 target = rec.p + rec.normal + random_in_unit_sphere();
+        return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth-1);
     }
     vec3 unit_direction = unit_vector(r.direction());
     auto t = 0.5 * (unit_direction.y() + 1.0);
@@ -26,6 +32,7 @@ int main(){
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width/aspect_ratio);
     const int samples_per_pixel = 100;
+    const int max_depth = 5;
 
     // World
     hittable_list world;
@@ -49,7 +56,7 @@ int main(){
               auto u = (i + random_double()) / (image_width-1);
               auto v = (j + random_double()) / (image_height-1);
               ray r = cam.get_ray(u, v);
-              pixel_color += ray_color(r, world);           
+              pixel_color += ray_color(r, world, max_depth);           
             }
             write_color(std::cout, pixel_color, samples_per_pixel);   
        }
--- vec3.h
+++ vec3.h
@@ -43,7 +43,15 @@ class vec3 {
 
         double length_squared() const {
             return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
-        }     
+        }    
+        inline static vec3 random(){
+            return vec3(random_double(), random_double(), random_double());
+        }
+
+        inline static vec3 random(double min, double max) {
+            return vec3(random_double(min, max), random_double(min, max), random_double(min,max));
+        }
+
 
     public:
         double e[3];
@@ -95,6 +103,16 @@ inline vec3 cross(const vec3 &u, const vec3 &v) {
 
 inline vec3 unit_vector(vec3 v) {
     return v / v.length();
+}
+
+
+
+vec3 random_in_unit_sphere(){
+    while(true){
+        auto p = vec3::random(-1, 1); // random vector in a cube
+        if (p.length_squared() >= 1) continue; // search different if vector length is greater than 1
+        return p;
+    }
 }
 
 #endif
