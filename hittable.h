b22fd7d0104f3439dd998b2c09aa10ea6435d1d2
AaryaGadekar <aaryagadekar01@gmail.com>
2023-07-11 18:30:59 +0530

    ground sphere rendered

--- hittable.h
+++ hittable.h
@@ -18,7 +18,7 @@ struct hit_record{
 
 class hittable {
     public:
-        virtual bool hit(const ray& r; double t_min, double t_max, hit_record& rec) const = 0;
+        virtual bool hit(const ray& r, double t_min, double t_max, hit_record& rec) const = 0;
 };
 
 
--- rtweekend.h
+++ rtweekend.h
@@ -21,6 +21,16 @@ inline double degrees_to_radians(double degrees){
     return degrees * pi / 180.0;
 }
 
+inline double random_double() {
+    // Returns a random real in [0,1).
+    return rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+    // Returns a random real in [min,max).
+    return min + (max-min)*random_double();
+}
+
 // Common Headers
 
 #include "ray.h"
