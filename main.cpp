c2d9d17adce89fe6b2369186e512711ff0e64d4f
AaryaGadekar <aaryagadekar01@gmail.com>
2023-07-11 09:32:09 +0530

    normals

--- main.cpp
+++ main.cpp
@@ -5,9 +5,29 @@
 
 #include <iostream>
 
+
+double hit_sphere(const point3& center, double radius, const ray& r){
+    vec3 oc = r.origin() - center;
+    auto a = dot(r.direction(), r.direction());
+    auto b = 2.0 * dot(oc, r.direction());
+    auto c = dot(oc, oc) - radius*radius;
+    auto discriminant = b*b - 4*a*c;
+    if (discriminant < 0){
+        return -1.0;
+    }else{
+        return (-b - sqrt(discriminant)) / (2.0*a); //solution to quadratic
+    }
+}
+
+
 color ray_color(const ray& r){
+    auto t = hit_sphere(point3(0,0,-1), 0.5, r);
+    if (t > 0.0){
+        vec3 N = unit_vector(r.at(t) - vec3(0,0,-1));
+        return 0.5*color(N.x()+1, N.y()+1, N.z()+1);
+    }
     vec3 unit_direction = unit_vector(r.direction());
-    auto t = 0.5*(unit_direction.y() + 1.0);
+    t = 0.5*(unit_direction.y() + 1.0);
     return (1.0-t) * color(1.0, 1.0, 1.0) + t*(color(0.5, 0.7, 1.0));
 }
 
@@ -38,11 +58,12 @@ int main(){
     for (int j = image_height-1; j >= 0; --j){
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i){
-        auto u = double(i) / (image_width-1);
-        auto v = double(j) / (image_height-1);
-        color pixel_color = ray_color(r);
-         write_color(std::cout, pixel_color);   
+            auto u = double(i) / (image_width-1);
+            auto v = double(j) / (image_height-1);
+            ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
+            color pixel_color = ray_color(r);
+            write_color(std::cout, pixel_color);   
        }
     }
-    cerr << "\nDone.\n";
+    std::cerr << "\nDone.\n";
 }
